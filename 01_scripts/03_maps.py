326ac3f098963b0791119daedfab8a5f4987377c
Alejandro Delgado <adelgado@iag.usp.br>
2022-01-17 17:20:35 -0500

    changes

--- 01_scripts/03_maps.py
+++ 01_scripts/03_maps.py
@@ -18,6 +18,7 @@ import xarray as xr
 import h5py
 from netCDF4 import Dataset
 import glob
+# import wrf
 import pytz
 #from mpl_toolkits.basemap import Basemap
 import cartopy.crs as ccrs
@@ -27,6 +28,27 @@ from mpl_toolkits.axes_grid1 import AxesGrid
 from cartopy.io.shapereader import Reader
 from cartopy.feature import ShapelyFeature
 
+#%% MDA8 diferences for September and October --------------------------------
+path = '../data/wrfout/'
+wrfout = [Dataset(i) for i in sorted(glob.glob(path+'wrfout_d02*'))]
+
+t2 = wrf.getvar(wrfout, 'T2', timeidx=wrf.ALL_TIMES, method='cat')
+rh2 = wrf.getvar(wrfout, 'rh2', timeidx=wrf.ALL_TIMES, method='cat')
+wind = wrf.getvar(wrfout, 'uvmet10_wspd_wdir', timeidx=wrf.ALL_TIMES, method='cat')
+ws = wind.sel(wspd_wdir='wspd')
+wd = wind.sel(wspd_wdir='wdir')
+psfc = wrf.getvar(wrfout, 'PSFC', timeidx=wrf.ALL_TIMES, method='cat')
+
+o3 = wrf.getvar(wrfout, 'o3', timeidx=wrf.ALL_TIMES, method='cat')
+o3_sfc  = o3.isel(bottom_top=0)
+R = 8.3144598 # J/K mol
+o3_u = o3_sfc * psfc * (16 * 3) / (R * t2)
+o3_u[0,:,:].plot()
+
+lon = t2.XLONG[0,:].values
+lat = t2.XLAT[:,0].values
+print(lon, lat)
+
 #%% Import processed data
 
 path = '02_data/processed/'
@@ -34,26 +56,28 @@ d02 = h5py.File(path + 'mapd02_201709.h5', 'r')
 d03 = h5py.File(path + 'mapd03_201709.h5', 'r')
 d04 = h5py.File(path + 'mapd04_201709.h5', 'r')
 
-#%%
 data = {
         "d02" : {
-            't2'  : d02.get('t2'),
-            'rh2' : d02.get('rh2'),
-            'wind': d02.get('wind')
+            't2'  : d02.get('t2')[3:,:,:],
+            'rh2' : d02.get('rh2')[3:,:,:],
+            'wind': d02.get('wind')[3:,:,:],
+            'lon' : d02.get('lon')
             },
         "d03" : {
-            't2'  : d03.get('t2'),
-            'rh2' : d03.get('rh2'),
-            'wind': d03.get('wind')
+            't2'  : d03.get('t2')[3:,:,:],
+            'rh2' : d03.get('rh2')[3:,:,:],
+            'wind': d03.get('wind')[3:,:,:]
             },
         "d04" : {
-            't2'  : d04.get('t2'),
-            'rh2' : d04.get('rh2'),
-            'wind': d04.get('wind')
+            't2'  : d04.get('t2')[3:,:,:],
+            'rh2' : d04.get('rh2')[3:,:,:],
+            'wind': d04.get('wind')[3:,:,:]
             }
         }
 
 for f in [d02, d03, d04]:
     f.close()
     
+#%%
+
 print(data['d02']['t2'].shape)
