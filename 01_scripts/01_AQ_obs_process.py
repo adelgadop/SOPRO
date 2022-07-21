6406f0bf42699f12b7da2bf0b3127d946f5e94bd
Alejandro Delgado <adelgado@iag.usp.br>
2022-07-21 16:07:05 -0300

    figures updating

--- 01_scripts/01_AQ_obs_process.py
+++ 01_scripts/01_AQ_obs_process.py
@@ -19,6 +19,28 @@ import matplotlib.ticker as plticker
 air = pd.read_csv('02_data/raw/air_quality/aq_data.csv', sep = ',', decimal='.')
 air['date'] = pd.to_datetime(air['date'], format = '%Y-%m-%d %H:%M').dt.tz_localize('Etc/GMT-1')
 
+#%% Figures particulate matter
+
+df_set = air.loc[(air.date >= "2017-09-01") & (air.date <= "2017-09-03"), :]
+names = list(air.name.unique())
+porto = names[0:2]
+lisbon = ['Olivais', 'Laranjeiro']
+lisbon2 = names[2:][4:]
+pols = list(air.columns[-6:])
+ylabel = '$\mu$gm$^{-3}$'
+ylabel_co = 'mg.m$^{-3}$'
+
+    
+fig, ax = plt.subplots(2, figsize = (6, 5), sharex = True, sharey = True)
+for i, n in enumerate(lisbon):
+    df_set.loc[df_set.name == n, :].plot(x = 'date', y=['pm10', 'pm2.5'], ax = ax[i], legend = False)
+    ax[i].set_title(n, loc = 'left', fontsize = 8)
+    ax[-len(lisbon)].legend(['PM$_{10}$', 'PM$_{2.5}$'])
+    fig.text(0.07, 0.5, ylabel, va = 'center', rotation='vertical')
+fig.savefig('03_output/fig/analysis/obs_pm_lisbon_set.png', bbox_inches='tight', facecolor='w', dpi=300)
+
+
+
 #%% Figures
 names = list(air.name.unique())
 porto = names[0:2]
@@ -36,15 +58,19 @@ for i, n in enumerate(porto):
     ax[len(porto)-1].legend(['PM$_{10}$', 'PM$_{2.5}$'])
     fig.text(0.07, 0.5, ylabel, va = 'center', rotation='vertical')
 
-fig.savefig('03_output/fig/analysis/obs_pm_porto.pdf', bbox_inches='tight', facecolor='w')
+fig.savefig('03_output/fig/analysis/obs_pm_porto.png', bbox_inches='tight', facecolor='w', dpi=300)
     
+#%%
+
 fig, ax = plt.subplots(len(lisbon), figsize = (12, 8), sharex = True, sharey = True)
 for i, n in enumerate(lisbon):
     air.loc[air.name == n, :].plot(x = 'date', y=['pm10', 'pm2.5'], ax = ax[i], legend = False)
     ax[i].set_title(n, loc = 'left', fontsize = 8)
     ax[-len(lisbon)].legend(['PM$_{10}$', 'PM$_{2.5}$'])
     fig.text(0.07, 0.5, ylabel, va = 'center', rotation='vertical')
-fig.savefig('03_output/fig/analysis/obs_pm_lisbon.pdf', bbox_inches='tight', facecolor='w')
+fig.savefig('03_output/fig/analysis/obs_pm_lisbon.png', bbox_inches='tight', facecolor='w', dpi=300)
+
+#%%
 
 fig, ax = plt.subplots(len(lisbon2), figsize = (12, 8), sharex = True, sharey = True)
 for i, n in enumerate(lisbon2):
@@ -52,7 +78,7 @@ for i, n in enumerate(lisbon2):
     ax[i].set_title(n, loc = 'left', fontsize = 8)
     ax[-len(lisbon)].legend(['PM$_{10}$', 'PM$_{2.5}$'])
     fig.text(0.07, 0.5, ylabel, va = 'center', rotation='vertical')
-fig.savefig('03_output/fig/analysis/obs_pm_lisbon2.pdf', bbox_inches='tight', facecolor='w')
+fig.savefig('03_output/fig/analysis/obs_pm_lisbon2.png', bbox_inches='tight', facecolor='w', dpi=300)
 
 fig, ax = plt.subplots(len(porto), figsize = (12, 4), sharex = True, sharey = True)
 for i, n in enumerate(porto):
@@ -62,7 +88,7 @@ for i, n in enumerate(porto):
     ax[len(porto)-1].legend(['NO$_2$', 'O$_3$'])
     fig.text(0.07, 0.5, ylabel, va = 'center', rotation='vertical')
 
-fig.savefig('03_output/fig/analysis/obs_no2_o3_porto.pdf', bbox_inches='tight', facecolor='w')
+fig.savefig('03_output/fig/analysis/obs_no2_o3_porto.png', bbox_inches='tight', facecolor='w', dpi=300)
     
 fig, ax = plt.subplots(len(lisbon), figsize = (12, 8), sharex = True, sharey = True)
 for i, n in enumerate(lisbon):
@@ -70,7 +96,7 @@ for i, n in enumerate(lisbon):
     ax[i].set_title(n, loc = 'left', fontsize = 8)
     ax[-len(lisbon)].legend(['NO$_2$', 'O$_3$'])
     fig.text(0.07, 0.5, ylabel, va = 'center', rotation='vertical')
-fig.savefig('03_output/fig/analysis/obs_no2_o3_lisbon.pdf', bbox_inches='tight', facecolor='w')
+fig.savefig('03_output/fig/analysis/obs_no2_o3_lisbon.png', bbox_inches='tight', facecolor='w', dpi=300)
 
 fig, ax = plt.subplots(len(lisbon2), figsize = (12, 8), sharex = True, sharey = True)
 for i, n in enumerate(lisbon2):
@@ -78,7 +104,7 @@ for i, n in enumerate(lisbon2):
     ax[i].set_title(n, loc = 'left', fontsize = 8)
     ax[-len(lisbon)].legend(['NO$_2$', 'O$_3$'])
     fig.text(0.07, 0.5, ylabel, va = 'center', rotation='vertical')
-fig.savefig('03_output/fig/analysis/obs_no2_o3_lisbon2.pdf', bbox_inches='tight', facecolor='w')
+fig.savefig('03_output/fig/analysis/obs_no2_o3_lisbon2.png', bbox_inches='tight', facecolor='w', dpi=300)
 
 fig, ax = plt.subplots(len(porto), figsize = (12, 4), sharex = True, sharey = True)
 for i, n in enumerate(porto):
@@ -88,7 +114,7 @@ for i, n in enumerate(porto):
     ax[len(porto)-1].legend(['CO'])
     fig.text(0.07, 0.5, ylabel_co, va = 'center', rotation='vertical')
 
-fig.savefig('03_output/fig/analysis/obs_co_porto.pdf', bbox_inches='tight', facecolor='w')
+fig.savefig('03_output/fig/analysis/obs_co_porto.png', bbox_inches='tight', facecolor='w', dpi=300)
     
 fig, ax = plt.subplots(len(lisbon), figsize = (12, 8), sharex = True, sharey = True)
 for i, n in enumerate(lisbon):
@@ -96,7 +122,7 @@ for i, n in enumerate(lisbon):
     ax[i].set_title(n, loc = 'left', fontsize = 8)
     ax[-len(lisbon)].legend(['CO'])
     fig.text(0.07, 0.5, ylabel_co, va = 'center', rotation='vertical')
-fig.savefig('03_output/fig/analysis/obs_co_lisbon.pdf', bbox_inches='tight', facecolor='w')
+fig.savefig('03_output/fig/analysis/obs_co_lisbon.png', bbox_inches='tight', facecolor='w', dpi=300)
 
 fig, ax = plt.subplots(len(lisbon2), figsize = (12, 8), sharex = True, sharey = True)
 for i, n in enumerate(lisbon2):
@@ -104,9 +130,11 @@ for i, n in enumerate(lisbon2):
     ax[i].set_title(n, loc = 'left', fontsize = 8)
     ax[-len(lisbon)].legend(['CO'])
     fig.text(0.07, 0.5, ylabel_co, va = 'center', rotation='vertical')
-fig.savefig('03_output/fig/analysis/obs_co_lisbon2.pdf', bbox_inches='tight', facecolor='w')
+fig.savefig('03_output/fig/analysis/obs_co_lisbon2.png', bbox_inches='tight', facecolor='w', dpi=300)
 
 #%%
+
+
 
 import folium
 
